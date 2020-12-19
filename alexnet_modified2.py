34d3db73c2122b4f69e0f847e6e8ac2d97ddce87
GianlucaMega <62256763+GianlucaMega@users.noreply.github.com>
2020-12-19 12:41:19 +0100

    Create alexnet_modified2.py

---
+++ alexnet_modified2.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+from torch.hub import load_state_dict_from_url
+from typing import Any
+from torch.autograd import Function
+
+__all__ = ['AlexNet', 'alexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+class ReverseLayerF(Function):
+    # Forwards identity
+    # Sends backward reversed gradients
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
+
+
+class alexnet2(nn.Module):
+    def __init__(self, **kwargs):
+        super(alexnet2, self).__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),)
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 1000),)
+        self.dann_classifier = nn.Sequential(nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 1000),)
+        state_dict = load_state_dict_from_url(model_urls['alexnet'],
+                                              progress=progress)
+        self.load_state_dict(state_dict,strict=False)
+
+    def forward(self, x, alpha=None):
+        features = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        # Flatten the features:
+        # features = features.view(features.size(0), -1)
+        # If we pass alpha, we can assume we are training the discriminator
+        if alpha is not None:
+            # gradient reversal layer (backward gradients will be reversed)
+            x= ReverseLayerF.apply(x, alpha)
+            discriminator_output = self.dann_classifier(x)
+            return discriminator_output
+        # If we don't pass alpha, we assume we are training with supervision
+        else:
+            class_outputs = self.classifier(x)
+            return class_outputs
